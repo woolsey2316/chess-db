32480f538fbd658931880d1078c671c6920c27c7
David <woolsey2316@gmail.com>
2024-10-16 11:27:05 +1100

    fix: object type

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
 }
 
